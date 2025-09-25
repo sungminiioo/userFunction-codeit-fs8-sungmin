b190a7868e96d7b79baa61d1aaf371a522fb24c3
sungminiioo <sungminiioo426@gmail.com>
2025-09-25 11:33:08 +0900

    실습 완료

--- src/components/pages/RegisterPage.js
+++ src/components/pages/RegisterPage.js
@@ -40,26 +40,26 @@ function RegisterPage() {
     try {
       setLoading(true);
       setError(null);
+      await authService.register(values.name, values.email, values.password);
+    //   const response = await fetch(
+    //     "https://learn.codeit.kr/api/link-service/users",
+    //     {
+    //       method: "POST",
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //       },
+    //       body: JSON.stringify({
+    //         name: values.name,
+    //         email: values.email,
+    //         password: values.password,
+    //       }),
+    //     },
+    //   );
 
-      const response = await fetch(
-        "https://learn.codeit.kr/api/link-service/users",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            name: values.name,
-            email: values.email,
-            password: values.password,
-          }),
-        },
-      );
-
-      if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error(errorData.message || "회원가입에 실패했습니다.");
-      }
+    //   if (!response.ok) {
+    //     const errorData = await response.json();
+    //     throw new Error(errorData.message || "회원가입에 실패했습니다.");
+    //   }
 
       // 회원가입 성공 후 처리
       alert("회원가입에 성공했습니다.");
--- src/lib/fetchClient.js
+++ src/lib/fetchClient.js
@@ -1,3 +1,5 @@
+import { headers } from "next/headers";
+
 /**
  * 기본 fetch 클라이언트 - 인증이 필요 없는 일반 요청용
  */
@@ -44,8 +46,13 @@ export const cookieFetch = async (url, options = {}) => {
   const defaultOptions = {
     // TODO: 실습내용
     // 1. 쿠키 전송을 위한 설정
+    Credentials: "include",
     // 2. 서버 컴포넌트에서도 매번 재검증을 cache 옵션
+    cache: "no-store",
     // 3. Content-Type 헤더 설정
+    headers: {
+      "Content-type": "application/json",
+    },
   };
 
   const mergedOptions = {
