832ec76e3fe94c990eb3d06a80c49fc57ae1a103
sungminiioo <sungminiioo426@gmail.com>
2025-09-25 10:42:39 +0900

    실습 완료

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -7,13 +7,16 @@ import Button from "../Button";
 import HorizontalRule from "../HorizontalRule";
 import Link from "next/link";
 import styles from "./LoginPage.module.css";
+import { useRouter } from "next/navigation";
 
 function LoginPage() {
   const [values, setValues] = useState({
     email: "",
     password: "",
   });
-
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const router = useRouter();
   function handleChange(e) {
     const { name, value } = e.target;
 
@@ -25,12 +28,40 @@ function LoginPage() {
 
   async function handleSubmit(e) {
     e.preventDefault();
-    // TODO: 로그인 처리
+        // TODO: 로그인 처리
     // 1. fetch 를 사용하여 로그인 요청을 보냅니다.
     // 2. 성공 시 Application 에 쿠키 내 토큰 저장 여부를 확인합니다.
     // 3. 로딩 상태를 만들고 로딩중일 때는 로그인 버튼을 비활성화 합니다.
     // 4. 추가로 로딩중일 때는 로그인 버튼텍스트를 "로그인 중..."으로 변경합니다.
     // 5. 에러 상태를 만들고 로그인 요청이 실패 시 에러 메시지를 로그인버튼 바로 위에 표시합니다.
+
+    try {
+      setError(null);
+      setLoading(true);
+      const response = await fetch(
+        "https://learn.codeit.kr/api/link-service/auth/login",
+        {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: values.email,
+            password: values.password,
+          }),
+        },
+      );
+      if (!response.ok) {
+        throw new Error("로그인에 실패했습니다.");
+      }
+      alert("로그인에 성공했습니다.");
+      router.push("/me");
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
   }
 
   return (
@@ -61,7 +92,10 @@ function LoginPage() {
           value={values.password}
           onChange={handleChange}
         />
-        <Button className={styles.Button}>로그인</Button>
+        {error && <div className={styles.Error}>{error}</div>}
+        <Button className={styles.Button} disabled={loading}>
+          {loading ? "로그인 중..." : "로그인"}
+        </Button>
         <HorizontalRule className={styles.HorizontalRule}>또는</HorizontalRule>
         <Button
           className={styles.GoogleButton}
